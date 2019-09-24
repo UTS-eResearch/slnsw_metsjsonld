f0af10955c56464879915ef672bed4cbc2c1d18e
Peter Sefton <peter.sefton@uts.edu.au>
2019-09-24 18:01:06 +1000

    Updated to match latest RO-Crate spec and added some comments

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const fs = require('fs-extra');
 const path = require('path');
 const xml2js = require('xml2js');
 
+
+// These constants need to be hooked up to your API
 const fileName = 'b1385';
-const bookName = 'Pioneer Work in the Alps of New Zealand';
+const bookName = 'Scannedpages';
 const bookJSONLD = 'ro-crate-metadata.jsonld';
+const name  = "Book title";
+const description = "Description";
+const datePublished = "2019-01-01";
 
 const fileLocation = path.join(process.cwd(), fileName + '.xml');
 const roCrateTemplate = fs.readJsonSync(path.join(process.cwd(), 'template', 'ro-crate-metadata.jsonld'));
@@ -19,6 +24,16 @@ fs.readFile(fileLocation, function (err, data) {
     const files = amets['mets:structMap'];
 
     jsonLD = roCrateTemplate;
+    // Find the root dataset
+    // First find the item that is the root dataset
+    var rootDatasetDescriptor = jsonLD["@graph"].filter((d) => {if (d["@id"]==["ro-crate-metadata.jsonld"]) {return d} })[0];
+    // Then find the rootDataset that it is about.
+    console.log("desc", rootDatasetDescriptor);
+    const rootDataset = jsonLD["@graph"].filter((r) => {if (r["@id"] ==  rootDatasetDescriptor.about["@id"]){ return r}})[0];
+
+    rootDataset.name = name;
+    rootDataset.description = description;
+    rootDataset.datePublished = datePublished;
 
     files.map((file, index) => {
       const image = file['mets:div'][0];
@@ -78,20 +93,29 @@ fs.readFile(fileLocation, function (err, data) {
       jsonLD['@graph'].push(pageObj);
 
     });
-
+    
     const book = {
-      '@id': 'book',
+      '@id': '_:book',
       'name': bookName,
-      '@type': 'book',
+      '@type': ['Dataset'],
       'hasPart': hasPart
     };
+    rootDataset.hasPart = [
+      {
+        '@id': '_:book'
+      }
+    ];
+    
+    // TODO if there are epub and PDF versions of the book add them too, as per
+    // This example: https://data.research.uts.edu.au/examples/ro-crate/examples/src/samples/IE4783007/ro-crate-metadata.jsonld
+
 
     jsonLD['@graph'].push(book);
 
     let data = JSON.stringify(jsonLD, null, 2);
 
     fs.writeFileSync(bookJSONLD, data, {mode: 0o755});
-
+    
     console.log(`done writing book jsonld : ${bookJSONLD}`);
   });
 
